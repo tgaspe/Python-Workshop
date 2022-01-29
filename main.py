20432c541a303f82b73200fcfb6fba6ec6fd950c
tgaspe <theodorogtc@gmail.com>
2022-01-29 11:31:57 -0500

    Second commit

---
+++ main.py
@@ -0,0 +1,10 @@
+def main_wrapper():
+    print("This is the start of our project. This function's name is {Main_wrapper.__name__}")
+
+    # Stuff here
+    #
+    
+    print("this is the end of the project")
+
+if __name__ == "__main__":
+    main_wrapper()
--- pull_request_template.md
+++ pull_request_template.md
@@ -1,4 +1,4 @@
-## Type 
-- [] New feature
-- [] Refactoring
+### Type 
+- [ ] New feature
+- [ ] Refactoring
 - 
