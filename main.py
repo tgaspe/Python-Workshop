db1fa4974a4567bcaf8daa7db0c45cec3fcffd89
tgaspe <98555209+tgaspe@users.noreply.github.com>
2022-01-29 11:05:16 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+def main_wrapper():
+    print("This is the start of our project. This function's name is {Main_wrapper.__name__}")
+    
+    
+    print("this is the end of the project")
+
+if __name__ == "__main__":
+    main_wrapper()
